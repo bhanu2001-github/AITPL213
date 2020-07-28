d63e73d2b49098bf06d331e2f690a1562f2dc84a
bhanu2001-github <68578626+bhanu2001-github@users.noreply.github.com>
2020-07-28 15:56:51 +0530

    Add files via upload
    commit

---
+++ python4.py
@@ -0,0 +1,9 @@
+str = " bhavana banglore karanataka india"
+str = input("bhavana banglore karanataka india")
+words = str.sort()
+words.sort()
+print("The sorted words are:")
+for word in words:
+    print(word)
+       
+
