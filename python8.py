84291b3f46a0aa85c616062d040d0a1ba44303c6
bhanu2001-github <68578626+bhanu2001-github@users.noreply.github.com>
2020-08-03 11:00:03 +0530

    Add files via upload
    commit

---
+++ python8.py
@@ -0,0 +1,14 @@
+inc = 1
+num = 1
+for x in range (5, 0,-4):
+    for y in range(x, 0,-4):
+        print("",end='')
+        print(str(num) *inc)
+        num+=1
+        inc+=1;
+
+1
+22
+333
+
+                          
